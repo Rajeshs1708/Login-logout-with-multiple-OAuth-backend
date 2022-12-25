b32bf2f0570f9927b964b46fdc7fe0665ea2fdf2
Rajesh <rajeshs1097@gmail.com>
2022-12-26 00:04:20 +0530

    second commit

--- passport.js
+++ passport.js
@@ -5,7 +5,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 
 const GOOGLE_CLIENT_ID        = "946149001874-vbkhl33mrjloffvhj60hl7uetmgl1n7q.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET    = "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"
-const GOOGLE_CALLBACK_URL     = "https://login-and-logout-with-multiple-oauth.onrender.com/auth/google/callback"
+const GOOGLE_CALLBACK_URL     = "/auth/google/callback"
 
 const GITHUB_CLIENT_ID        = "14f0d5998bc2936f3c1b"
 const GITHUB_CLIENT_SECRET    = "ba7dc23142e6874aaa69379afb1807ca5576708c"
