98cd7a7ea2e6c2a8f4819f4b7a29ecf4faeb0ae4
Rajesh <rajeshs1097@gmail.com>
2022-12-25 16:11:12 +0530

    second commit

--- passport.js
+++ passport.js
@@ -5,13 +5,13 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 
 const GOOGLE_CLIENT_ID = "946149001874-vbkhl33mrjloffvhj60hl7uetmgl1n7q.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
-const GOOGLE_CALLBACK_URL = "http://localhost:8080/auth/google/callback"
+const GOOGLE_CALLBACK_URL = `https://login-and-logout-with-multiple-oauth.onrender.com/auth/google/callback`
 const GITHUB_CLIENT_ID = "14f0d5998bc2936f3c1b"
 const GITHUB_CLIENT_SECRET = "ba7dc23142e6874aaa69379afb1807ca5576708c"
-const GITHUB_CALLBACK_URL = "http://localhost:8080/auth/github/callback"
+const GITHUB_CALLBACK_URL = "https://login-and-logout-with-multiple-oauth.onrender.com/auth/github/callback"
 const FACEBOOK_APP_ID = "asdadsad"
 const FACEBOOK_APP_SECRET = "asdasdasd"
-const FACEBOOK_CALLBACK_URL = "http://localhost:8080/auth/facebook/callback"
+const FACEBOOK_CALLBACK_URL = "https://login-and-logout-with-multiple-oauth.onrender.com/auth/facebook/callback"
 
 passport.serializeUser((user, done) => {
   done(null, user)
