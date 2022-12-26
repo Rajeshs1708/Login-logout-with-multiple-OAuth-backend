cab7a99ecd534e18d11d4305ec9c42041277bebd
Rajesh <rajeshs1097@gmail.com>
2022-12-26 15:46:52 +0530

    second commit

--- passport.js
+++ passport.js
@@ -3,18 +3,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 
-const GOOGLE_CLIENT_ID        = "946149001874-vbkhl33mrjloffvhj60hl7uetmgl1n7q.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET    = "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"
-const GOOGLE_CALLBACK_URL     = "/auth/google/callback"
-
-const GITHUB_CLIENT_ID        = "14f0d5998bc2936f3c1b"
-const GITHUB_CLIENT_SECRET    = "ba7dc23142e6874aaa69379afb1807ca5576708c"
-const GITHUB_CALLBACK_URL     = "/auth/github/callback"
-
-const FACEBOOK_APP_ID         = "1f0d5998bc2936f3"
-const FACEBOOK_APP_SECRET     = "23142e6874aaa69379afb1807c"
-const FACEBOOK_CALLBACK_URL   = "/auth/facebook/callback"
-
 passport.serializeUser((user, done) => {
   done(null, user)
 })
@@ -24,9 +12,9 @@ passport.deserializeUser((user, done) => {
 
 // Google
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: GOOGLE_CALLBACK_URL,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback",
 },
   function (request, accessToken, refreshToken, profile, done) {
     return done(null, profile)
@@ -35,9 +23,9 @@ passport.use(new GoogleStrategy({
 
 // Github
 passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: GITHUB_CALLBACK_URL,
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "/auth/github/callback",
 },
   function (request, accessToken, refreshToken, profile, done) {
     return done(null, profile);
@@ -46,9 +34,9 @@ passport.use(new GitHubStrategy({
 
 // Facebook
 passport.use(new FacebookStrategy({
-  clientID: FACEBOOK_APP_ID,
-  clientSecret: FACEBOOK_APP_SECRET,
-  callbackURL: FACEBOOK_CALLBACK_URL,
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "/auth/facebook/callback",
 },
   function (request, accessToken, refreshToken, profile, done) {
     return done(null, profile);
