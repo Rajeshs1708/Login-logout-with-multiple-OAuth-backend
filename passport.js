a1fd23b798510da7672689f19c61e9ea3d14667c
Rajesh <rajeshs1097@gmail.com>
2023-01-09 13:19:33 +0530

    second commit

--- passport.js
+++ passport.js
@@ -14,7 +14,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "https://login-with-oauth.netlify.app/auth/google/callback",
 },
   function (request, accessToken, refreshToken, profile, done) {
     return done(null, profile)
