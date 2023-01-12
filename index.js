1cd32f024b9a7fae06edefeb20b29eee45e6d83a
Rajesh <rajeshs1097@gmail.com>
2023-01-12 13:53:47 +0530

    third commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: "https://login-with-oauth.netlify.app",
+    origin: "http://localhost:3000",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
