0829adfe7f963d02a4508bb7bc4519a2ba16c04b
Rajesh <rajeshs1097@gmail.com>
2023-01-12 14:12:39 +0530

    third commit

--- passport.js
+++ passport.js
@@ -14,7 +14,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "https://login-logout-with-multiple-o-auth-backend.vercel.app/auth/google/callback",
 },
   function (request, accessToken, refreshToken, profile, done) {
     console.log(profile);
